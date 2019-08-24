43e26ef4e2dadd7d23d7cfad67b3e2240ddf0d14
Jaikarthik <jaikarthik99@gmail.com>
2019-08-24 09:58:39 +0530

    comments added

--- minesweeper/mainFunction.py
+++ minesweeper/mainFunction.py
@@ -1,25 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from placingMines import cell_indication, mines_matrix, mines_index
 import mineSweeperUI as ui
+import tkinter as tk
 
-
-# In[4]:
-
-
+#Displays play of the cell once its clicked
 def showGrid(x,y):
     row1 = ui.button_list[x][y].grid_info()['row']      # Row of the button
     column1 = ui.button_list[x][y].grid_info()['column']   # grid_info will return dictionary with all grid elements (row, column, ipadx, ipday, sticky, rowspan and columnspan)
     print("Grid position of 'btn': {} {}".format(row1, column1))
-    #print(mines_matrix[x][y])
-    ui.btn_texts[x][y].set(str(x)+','+str(y))
-    ui.btn_texts[x][y].set(cell_indication[x][y])
     zero_list = []
 
+    #Cell has mine around it. Indicates the number of mines.
+    if(cell_indication[x][y] != '*' and cell_indication[x][y] != 0 ):
+        ui.btn_texts[x][y].set(cell_indication[x][y])
+
+    #Cell has mine - Game ends
     if(cell_indication[x][y] == '*'):
         for a in mines_index:
             x = a[0]
@@ -31,9 +25,14 @@ def showGrid(x,y):
             for y in range(10):
                 ui.button_list[x][y].configure(state = 'disabled')
 
+        game_over_window = tk.Tk()
+        game_over_window.title('Game over!')
+        LARGE_FONT= ("Verdana", 12)
+        label = tk.Label(game_over_window, text="Oh no!! You stepped on a Mine!! Game over :(", font = LARGE_FONT)
+        label.pack()
+        game_over_window.mainloop()
 
-
-
+    #Cell has no mines around. Add them to Zero mine list and open all cell around it
     if(cell_indication[x][y] == 0 and [x,y] not in zero_list):
         zero_list.append([x,y])
         for a in zero_list:
@@ -51,9 +50,6 @@ def showGrid(x,y):
                     if(cell_indication[x-1][y+1] == 0 and [x-1,y+1] not in zero_list):
                         zero_list.append([x-1,y+1])
 
-
-
-
             if(x>=0 and x<=9):
                 if(y-1 >=0):
                     if(cell_indication[x][y-1] == 0 and [x,y-1] not in zero_list):
@@ -61,8 +57,6 @@ def showGrid(x,y):
                 if(y+1<=9):
                     if(cell_indication[x][y+1] == 0 and [x,y+1] not in zero_list):
                         zero_list.append([x,y+1])
-
-
 
             if(x+1 <= 9):
                 if(y-1 >=0):
--- minesweeper/placingMines.py
+++ minesweeper/placingMines.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import random
-
-
-# In[ ]:
-
 
 x_mine = []
 y_mine = []
@@ -17,26 +7,16 @@ for _ in range(10):
     x_mine.append(random.randint(0,9))
     y_mine.append(random.randint(0,9))
 
-
-# In[ ]:
-
-
+#Random placing mines in the cell
 mines_matrix = [[0]*10 for y in range(10)]
-mines_index = [] 
+mines_index = []
 for i in range(10):
     mines_matrix[x_mine[i]][y_mine[i]] = 1
     mines_index.append([x_mine[i],y_mine[i]])
 
-
-# In[ ]:
-
-
 cell_indication = [[0]*10 for y in range(10)]
 
-
-# In[ ]:
-
-
+#Adding values to cell based on the mines around them 0,1,2,3,4,5,6
 for x in range(10):
     for y in range(10):
         if (mines_matrix[x][y] == 0):
@@ -49,7 +29,7 @@ for x in range(10):
                     cell_indication[x][y] =mines_matrix[x][y-1] +  mines_matrix[x+1][y-1] + mines_matrix[x+1][y]
             elif(x>0 and x<9):
                 if(y==0):
-                    cell_indication[x][y] =mines_matrix[x-1][y] + mines_matrix[x-1][y+1] +  mines_matrix[x][y+1] + mines_matrix[x+1][y] + mines_matrix[x+1][y+1] 
+                    cell_indication[x][y] =mines_matrix[x-1][y] + mines_matrix[x-1][y+1] +  mines_matrix[x][y+1] + mines_matrix[x+1][y] + mines_matrix[x+1][y+1]
                 elif(y>0 and y<9):
                     cell_indication[x][y] =mines_matrix[x-1][y-1] + mines_matrix[x-1][y] + mines_matrix[x-1][y+1] + mines_matrix[x][y-1] + mines_matrix[x][y+1] + mines_matrix[x+1][y-1] + mines_matrix[x+1][y] + mines_matrix[x+1][y+1]
 
@@ -60,16 +40,9 @@ for x in range(10):
                     cell_indication[x][y] =mines_matrix[x-1][y] + mines_matrix[x-1][y+1]  + mines_matrix[x][y+1]
 
                 elif(y>0 and y<9):
-                    cell_indication[x][y] =mines_matrix[x-1][y-1] + mines_matrix[x-1][y] + mines_matrix[x-1][y+1]+ mines_matrix[x][y-1]  + mines_matrix[x][y+1]                                
+                    cell_indication[x][y] =mines_matrix[x-1][y-1] + mines_matrix[x-1][y] + mines_matrix[x-1][y+1]+ mines_matrix[x][y-1]  + mines_matrix[x][y+1]
 
                 elif(y==9):
-                    cell_indication[x][y] =mines_matrix[x-1][y-1] + mines_matrix[x-1][y] + mines_matrix[x][y-1] 
+                    cell_indication[x][y] =mines_matrix[x-1][y-1] + mines_matrix[x-1][y] + mines_matrix[x][y-1]
         else:
             cell_indication[x][y] ='*'
-
-
-# In[ ]:
-
-
-
-
