52b32e9dd2e50b0be8ed6eb86f8a3b89f4311345
Jaikarthik <jaikarthik99@gmail.com>
2019-08-24 09:10:37 +0530

    commented some print

--- minesweeper/mainFunction.py
+++ minesweeper/mainFunction.py
@@ -19,27 +19,27 @@ def showGrid(x,y):
     ui.btn_texts[x][y].set(str(x)+','+str(y))
     ui.btn_texts[x][y].set(cell_indication[x][y])
     zero_list = []
-    
+
     if(cell_indication[x][y] == '*'):
         for a in mines_index:
             x = a[0]
             y = a[1]
             ui.btn_texts[x][y].set(cell_indication[x][y])
             ui.button_list[x][y].configure(bg = 'red')
-            
+
         for x in range(10):
             for y in range(10):
                 ui.button_list[x][y].configure(state = 'disabled')
-            
-            
-    
-   
+
+
+
+
     if(cell_indication[x][y] == 0 and [x,y] not in zero_list):
         zero_list.append([x,y])
         for a in zero_list:
             x = a[0]
             y = a[1]
-                   
+
             if(x-1 >= 0):
                 if(y-1 >=0):
                     if(cell_indication[x-1][y-1] == 0 and [x-1,y-1] not in zero_list):
@@ -74,8 +74,8 @@ def showGrid(x,y):
                 if(y+1<=9):
                     if(cell_indication[x+1][y+1] == 0 and [x+1,y+1] not in zero_list):
                         zero_list.append([x+1,y+1])
-            
-        
+
+
         for a in zero_list:
             x = a[0]
             y = a[1]
@@ -84,17 +84,17 @@ def showGrid(x,y):
                     ui.btn_texts[x-1][y-1].set("" if cell_indication[x-1][y-1]==0 else cell_indication[x-1][y-1])
                     ui.button_list[x-1][y-1].configure( state= 'disabled', bg = 'lightgrey')
 #                     btn_texts[x-1][y-1].set(cell_indication[x-1][y-1])
-                    
+
                 if(y>=0 and y<=9):
                     ui.btn_texts[x-1][y].set("" if cell_indication[x-1][y] == 0 else cell_indication[x-1][y])
                     ui.button_list[x-1][y].configure(state= 'disabled', bg = 'lightgrey')
 #                     btn_texts[x-1][y].set(cell_indication[x-1][y])
-                    
+
                 if(y+1<=9):
                     ui.btn_texts[x-1][y+1].set("" if cell_indication[x-1][y+1] == 0 else cell_indication[x-1][y+1])
                     ui.button_list[x-1][y+1].configure( state= 'disabled', bg = 'lightgrey')
 #                     btn_texts[x-1][y+1].set(cell_indication[x-1][y+1])
-                   
+
 
 
 
@@ -103,12 +103,12 @@ def showGrid(x,y):
                     ui.btn_texts[x][y-1].set("" if cell_indication[x][y-1] == 0 else cell_indication[x][y-1])
                     ui.button_list[x][y-1].configure( state= 'disabled', bg = 'lightgrey')
                    # btn_texts[x][y-1].set(cell_indication[x][y-1])
-                   
+
                 if(y+1<=9):
                     ui.btn_texts[x][y+1].set("" if cell_indication[x][y+1] == 0 else cell_indication[x][y+1])
                     ui.button_list[x][y+1].configure( state= 'disabled', bg = 'lightgrey')
                     #btn_texts[x][y+1].set(cell_indication[x][y+1])
-                    
+
 
 
 
@@ -117,25 +117,19 @@ def showGrid(x,y):
                     ui.btn_texts[x+1][y-1].set("" if cell_indication[x+1][y-1] == 0 else cell_indication[x+1][y-1])
                     ui.button_list[x+1][y-1].configure( state= 'disabled', bg = 'lightgrey')
                     #btn_texts[x+1][y-1].set(cell_indication[x+1][y-1])
-                  
+
                 if(y>=0 and y<=9):
                     ui.btn_texts[x+1][y].set("" if cell_indication[x+1][y] == 0 else cell_indication[x+1][y])
                     ui.button_list[x+1][y].configure( state= 'disabled', bg = 'lightgrey')
                     #btn_texts[x+1][y].set(cell_indication[x+1][y])
-                    
+
                 if(y+1<=9):
                     ui.btn_texts[x+1][y+1].set("" if cell_indication[x+1][y+1] ==0 else cell_indication[x+1][y+1])
                     ui.button_list[x+1][y+1].configure( state= 'disabled', bg = 'lightgrey')
                     #btn_texts[x+1][y+1].set(cell_indication[x+1][y+1])
-                    
-    print(zero_list)
-    print(len(zero_list))
-    
-   
 
-
-# In[ ]:
-
+    #print(zero_list)
+    #print(len(zero_list))
 
 
 
@@ -151,3 +145,5 @@ def showGrid(x,y):
 
 
 
+
+# In[ ]:
